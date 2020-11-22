1ce60491ff24be8933969896538093eef5825353
Amit Jonas <amitjonas1@gmail.com>
2020-11-22 14:11:02 +0200

    added methods

--- testpack9/testfile9(Car).py
+++ testpack9/testfile9(Car).py
@@ -5,13 +5,20 @@ It has functions, which include parameters that are related to the class when I 
 class Car:
     def beep(self):
         print("I am a new car!",self.id, self.type, self.color, self.km, self.feul, self.tank)
-"""We can also add a parameter to a function in a class, that can relate to a different function in the same class:"""
-def add_feul(self, feul):
-    self.feul+=feul
-    if self.feul>self.tank:
-        self.feul=self.tank
-"""We can add a condition to a function as well."""
-"""We can use bullean logic to our advantage:"""
-def need_feul(self):
-    if self.feul<5:
-        return True
+    """We can also add a parameter to a function in a class, that can relate to a different function in the same class:"""
+    def add_feul(self, feul):
+        self.feul+=feul
+        if self.feul>self.tank:
+            self.feul=self.tank
+    """We can add a condition to a function as well."""
+    """We can use bullean logic to our advantage:"""
+    def need_feul(self):
+        if self.feul<5:
+            return True
+    """A constructor is a special method that starts before the object."""
+    def __init__(self):
+        print("hello")
+    """The __str__ function prints the str value of a function, instead of printing the location
+    of that function in memory."""
+    def __str__(self):
+        return f"{self.id}, {self.type}"
